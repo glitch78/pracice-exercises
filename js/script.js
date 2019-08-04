73119a4bb4f21157bf5d714ff77e178437056c54
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-08-04 17:34:24 -0500

    add practice js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var  firstName = "Lancelot";
+console.log(firstName);
+
+var lastName = "Ellis";
+
+var age = 45;
+var fullAge = true;
+console.log(fullAge);
+
+var  job;
+console.log(job);
+
+job = "teacher";
+console.log(job);
