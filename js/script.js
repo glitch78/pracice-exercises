2ae99f4d7fb8a51b30bb767b05663d2682896b71
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-08-04 19:22:51 -0500

    add variables and operators lesson from codeup

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ console.log(job);
  */
 
 
-
+/*
 var firstName = "Lancelot ";
 var age = 45;
 
@@ -38,7 +38,7 @@ console.log(firstName + " " + age);
 var job, isMarried;
 job = "teacher";
 isMarried = false;
-console.log(firstName + " is a " + age + "year old " + job + ". is he married? " + isMarried);
+console.log(firstName + " is a " + age + " year old " + job + ". is he married? " + isMarried);
 
 // variable mutation
 age = "twenty eight ";
@@ -47,7 +47,84 @@ job = "driver";
 alert(age + job);
 
 var lastName = prompt("what is his last name ");
-console.log(firstName + lastName);
+console.log(firstName + lastName + age);
+*/
+
+/*******************************
+ * Basic operators
+ */
+var year, yearJohn, yearMark;
+var now = 2019;
+var ageJohn = 28;
+var ageMark = 32;
+
+
+// math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+//logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeOf operator
+console.log(typeof johnOlder);
+
+var  littleMermaid, brotherBear, hercules, day;
+
+littleMermaid = 3;
+brotherBear = 5;
+hercules = 1;
+day = 3;
+
+//variables and operators lesson from codeup
+var mermaidTotal = littleMermaid * day;
+var brotehrBearTotal = brotherBear * day;
+var herculesTotal = hercules * day;
+
+console.log(mermaidTotal + brotehrBearTotal + herculesTotal);
+
+var rentalTotal = mermaidTotal + brotehrBearTotal + herculesTotal;
+console.log(rentalTotal);
+
+var google, amazon, faceBook, googleHours, amazonHours, faceBookHours, googlePay, amazonPay, faceBookPay, totalPay;
+google = 400;
+amazon = 380;
+faceBook = 350;
+googleHours = 6;
+amazonHours = 4;
+faceBookHours = 10;
+
+googlePay = google * googleHours;
+
+console.log(googlePay);
+
+amazonPay = amazon * amazonHours;
+
+faceBookPay = faceBook * faceBookHours;
+
+totalPay = googlePay + amazonPay + faceBookPay;
+
+console.log(totalPay);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
